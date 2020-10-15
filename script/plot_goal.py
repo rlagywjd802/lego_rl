781543e47fa4f73df0c855043e8e302e404bdd6d
hyojeong <rlagywjd802@gmail.com>
2020-10-15 20:55:00 +0900

    add ply file

--- script/plot_goal.py
+++ script/plot_goal.py
@@ -9,8 +9,8 @@ Demonstrates plotting 3D volumetric objects with ``ax.voxels``
 import matplotlib.pyplot as plt
 import numpy as np
 
-# This import registers the 3D projection, but is otherwise unused.
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+def ply_to_voxel(file_path):
+    txt_file = open(file_path)
 
 
 # prepare some coordinates
@@ -20,7 +20,7 @@ x, y, z = np.indices((10, 10, 10))
 # cube1 = (x < 3) & (y < 3) & (z < 3)
 # cube2 = (x >= 5) & (y >= 5) & (z >= 5)
 # link = abs(x - y) + abs(y - z) + abs(z - x) <= 2
-goal =
+goal = ply_to_voxel('dog.ply')
 
 # combine the objects into a single boolean array
 voxels = goal
